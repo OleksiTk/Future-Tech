720b4528d766b2faaf8072cea6b7be858311d59a
Oleksii <aatswim1@gmail.com>
2025-01-09 17:48:18 +0200

    finish new page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import Header from "./headers.js";
 import TabsCollection from "./tabs.js";
+import VideoPlayerCollection from "./VideoPlayer.js";
 //
 
 new Header();
 new TabsCollection();
+new VideoPlayerCollection();
